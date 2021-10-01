d5b8b1bdbbe21e2207d69b3f842f58a4ae1b9a4f
Matheus Araujo <matheus.araujo.net@hotmail.com>
2021-10-01 18:13:52 -0300

    Projeto Local Criado

---
+++ src/Application/Main.java
@@ -0,0 +1,24 @@
+package Application;
+
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.stage.Stage;
+
+public class Main extends Application {
+    
+    @Override
+    public void start(Stage primaryStage) {
+        StackPane root = new StackPane();
+        
+        Scene scene = new Scene(root, 300, 250);
+        
+        primaryStage.setScene(scene);
+        primaryStage.show();
+    }
+
+    public static void main(String[] args) {
+        launch(args);
+    }
+    
+}
